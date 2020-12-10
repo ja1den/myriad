6e8d0053896a067243baccc164a3149cac73a7ee
Jaiden Douglas <jaidentdouglas@gmail.com>
2020-12-10 12:44:54 +1030

    feat: add token validation

--- src/index.ts
+++ src/index.ts
@@ -2,29 +2,36 @@
 import Discord from 'discord.js';
 import 'colors';
 
+// Handle Exit
+process.on('exit', () => console.log());
+
+// Token
+const token = process.env.DISCORD_TOKEN;
+
+if (token === undefined) {
+	console.log('DISCORD_TOKEN cannot be undefined.'.red);
+	process.exit(1);
+}
+
 // Main
-const client = new Discord.Client();
-
-client.on('ready', () => {
-	console.log('Logged in as', client.user?.tag.cyan);
-	console.log(`\u2728  Guilds: ${client.guilds.cache.size}`.green);
-
-	client.user
-		?.setPresence({
-			activity: { type: 'LISTENING', name: 'm!help' },
-			status: 'online',
-		})
-		.catch(console.error);
+const client = new Discord.Client({
+	presence: {
+		activity: { type: 'LISTENING', name: 'm!help' },
+		status: 'online',
+	},
 });
 
-client.login(process.env.DISCORD_TOKEN);
+client.on('ready', () => {
+	console.log('Logged in as', client.user?.username.cyan);
+	console.log(`\u2728  Guilds: ${client.guilds.cache.size}`.green);
+});
+
+client.login(token);
 
 // Handle Exit
 for (const signal of ['SIGINT', 'SIGTERM', 'SIGUSR2']) {
 	process.on(signal, () => {
-		client?.destroy();
+		client.destroy();
 		process.exit();
 	});
 }
-
-process.on('exit', () => console.log());
